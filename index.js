06acf06a1bbfba5b04701b1314eb0aef1d79335e
cr1tterp0wer <capodacac@gmail.com>
2020-08-28 18:53:37 -0500

    added express

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
-var cc = require('chromecast-api');
+/**
+ * REST API to forward chromecast commands
+ * Christopher Apodaca
+ */
 
+const express = require('express');
+const PORT = 9898;
+const server = express();
+const ChromecastAPI = require('chromecast-api');
+const client = new ChromecastAPI();
+const app = express();
 
-document.body.style.border = '5px solid red';
+/**
+ * POST: /play
+ * Play media from url: req.body.mediaURL
+ * @params {Object} req - request from client
+ * @params {Object} res - response to client
+ */
+app.post('/play', (req, res) => {
+	res.status(200).send('Success');
+});
+
+/**
+ * GET: /
+ * Establish PING PONG
+ * @params {Object} req - request from client
+ * @params {Object} res - response to client
+ */
+app.get('/', (req, res) => {
+	res.status(200).send('Success');
+});
+
+app.listen(PORT, () => {
+	console.log('Firecast up on ' + PORT + '!');
+});
